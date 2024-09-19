160a1220da17f508bc5ed92e6c840cbfd62d4e03
Ben Hertzler <bwhertzler@outlook.com>
2024-09-19 19:10:00 -0400

    Add project files.

---
+++ CSC 222 Interactive IO Project.cpp
@@ -0,0 +1,15 @@
+// CSC 222 Interactive IO Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Benjamin Hertzler
+// Mathematical Expressions and Interactive I/O Programming Project
+// Master
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
