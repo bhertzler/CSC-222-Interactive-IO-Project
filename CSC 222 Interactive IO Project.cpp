f5655c9b6d62fbea6240581720cc0a2e427aad8e
Ben Hertzler <bwhertzler@outlook.com>
2024-09-20 18:23:46 -0400

    initial commit

--- CSC 222 Interactive IO Project.cpp
+++ CSC 222 Interactive IO Project.cpp
@@ -3,13 +3,46 @@
 
 // Benjamin Hertzler
 // Mathematical Expressions and Interactive I/O Programming Project
-// Master
+// Project 2: Interest Earned
+
+// Description: This program calculates the amount of interest accrued by a savings account.
+// Required User Input: Principal, interest rate, number of times the interest is compounded.
+// Program Output: A report including the above numbers as well as the current balance in the account.
+// Supplied Constants: None.
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Variables
+    double balance, principal, rate, r, time_rate, interest;
+    int t;
+
+    cout << "Savings Interest" << endl;
+    cout << endl << "Principal: $";
+    cin >> principal;
+    cout << "Interest Rate: (%) ";
+    cin >> rate;
+    cout << "Total number of times compounded: ";
+    cin >> t;
+
+    r = rate / 100;
+    time_rate = 1 + (r / t);
+    time_rate = pow(time_rate, t);
+
+    balance = principal * time_rate;
+    interest = balance - principal;
+
+    cout << fixed << setprecision(2) << endl;
+    cout << left << setw(20) << "Interest Rate: " << right << setw(11) << rate << "%" << endl;
+    cout << left << setw(20) << "Times Compounded: " << right << setw(12) << t << endl;
+    cout << left << setw(20) << "Principal: " << right << '$' << setw(11) << principal << endl;
+    cout << left << setw(20) << "Interest: " << right << '$' << setw(11) << interest << endl;
+    cout << left << setw(20) << "Amount in Savings: " << right << '$' << setw(11) << balance << endl;
+
 }
 
 
